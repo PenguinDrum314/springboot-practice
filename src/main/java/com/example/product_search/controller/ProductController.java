dce57ed9386b5dcd1033e52c664892528b0f260e
PenguinDrum314 <pipipikku@gmail.com>
2026-09-14 15:17:35 +0900

    検索中調整中

--- src/main/java/com/example/product_search/controller/ProductController.java
+++ src/main/java/com/example/product_search/controller/ProductController.java
@@ -1,14 +1,22 @@
 package com.example.product_search.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
-	@GetMapping("/prodcuts/search")
-	public String search(@RequestParam("keyword") String keyword) {
-		return "検索キーワード" + keyword;
+	private final ProductService search();
+
+	public ProductController(ProductService productService) {
+	    this.search() = List<String>;
+	}
+	
+	@GetMapping("/products")
+	public List<String> findAll() {
+		return 
+				productService.findAll();
 	}
 	
 
--- src/main/java/com/example/product_search/service/ProductService.java
+++ src/main/java/com/example/product_search/service/ProductService.java
@@ -1,10 +1,12 @@
 package com.example.product_search.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
-	public String search(String keyword) {
-		return "検索キーワード" + keyword;
+	public List<String> search() {
+		return List.of("pen","notebook","pencil case");
 	}
 }
